4859fc1c37556b35c0db923ca4e1d89dc752078a
Ji-Haeng Huh <jhhuh.note@gmail.com>
2017-02-19 10:38:10 +0000

    git clone vundle in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-script_dir=$(dirname "$(readlink -f "$0")")
+script_dir=$(cd $(dirname $0);pwd -P) # Don't worry. $() evaluates the command in a subshell.
 home_dir=$HOME
 
 for fn in bashrc bash_profile bash_aliases emacs ghci tmux.conf vimrc vimrc.vundle
 do
     ln -s $script_dir/$fn $home_dir/.$fn
 done
+
+git clone https://github.com/VundleVim/Vundle.vim.git $home_dir/.vim/bundle/Vundle.vim 
+
