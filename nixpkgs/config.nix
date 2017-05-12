153a9684f06c489b850c2785a4ae4770e8c1cfe1
Ji-Haeng Huh <jhhuh.note@gmail.com>
2017-05-12 13:55:02 +0200

    change in nixpkgs/config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -16,50 +16,27 @@ in {
 
   packageOverrides = pkgs: rec {
 
-    tcplay = callPackage ./tcplay {};
-
-    speechd = pkgs.speechd.override { withEspeak = true; };
-
     inherit (nightly) yasr;
 
+    my-python = pkgs.python3.withPackages (
+      pypkgs: with pypkgs; [
+        jupyter numpy scipy matplotlib ]);
+
+    tcplay = callPackage ./tcplay {};
+    speechd = pkgs.speechd.override { withEspeak = true; };
     flite = nightly.flite.overrideDerivation (attr:{
       nativeBuildInputs = [ pkgs.alsaLib.dev ];
       configureFlags = attr.configureFlags + " --with-audio=alsa";
       LDFLAGS = "-L ${pkgs.alsaLib.dev}/lib";
     });
-
     eflite = nightly.eflite.override { flite = flite; debug = true; };
 
-   # WIP
+       # WIP
     rustNightly = pkgs.recurseIntoAttrs (nightly.callPackage (nightlyPath+"/pkgs/development/compilers/rust/nightly.nix") {});
-
     alacritty = callPackage ./alacritty {
       inherit (pkgs.xorg) libXcursor libXxf86vm libXi;
       rustPlatform = pkgs.makeRustPlatform rustNightly;
     };
 
-    # FAILED!!!!
-    flite_alsa = pkgs.flite.overrideDerivation (attr:{ 
-      configureFlags = "--enable-shared --with-audio=alsa";
-      buildInputs = with pkgs; [ pkgconfig alsaLib alsaLib.dev ];
-      CPPFLAGS = "-I ${pkgs.alsaLib.dev}/include";
-      LDFLAGS = "-L ${pkgs.alsaLib}/lib";
-    }); # enableOSSEmulation should be off. FAILED!
-
-    bluealsa = callPackage ./bluez-alsa/HEAD.nix {
-      automake = pkgs.automake.overrideDerivation (attr:{ patches = [ ./automake115x.patch ]; });
-    };
-
-    bluealsa_debug = bluealsa.overrideDerivation (attr:{
-      configureFlags = attr.configureFlags+" --enable-debug";
-    });
-
-    haskellPackages7103 = pkgs.haskell.packages.ghc7103.override {
-      overrides = self: super: rec { 
-        gtk2hs-buildtools = super.gtk2hs-buildtools.override {
-          Cabal = self.Cabal_1_24_2_0;
-        };
-      };
-    };
   };
 }
