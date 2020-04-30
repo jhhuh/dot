a230e5c9c5c6cc33ac8d97d7b22c00bc5c8dc804
Ji-Haeng Huh <jhhuh.note@gmail.com>
2020-05-01 00:00:12 +0900

    bump up nixos version 19.09 -> 20.03

--- update_nix-channel-git-revision.sh
+++ update_nix-channel-git-revision.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-channel="nixos-19.09"
+channel="nixos-20.03"
 rev=$(curl -L https://nixos.org/channels/"$channel"/git-revision | tee ./git-revision_nixpkgs)
 
 source ./update_nix-channel.sh
