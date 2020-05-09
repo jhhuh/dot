0440b638118f0d35d8e32c95f3ab3a0d46ff374c
Ji-Haeng Huh <jhhuh.note@gmail.com>
2020-05-09 23:18:12 +0900

    make xmonad always built with system nixos channel

--- xmonad/ghci.sh
+++ xmonad/ghci.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-nix-shell -p "haskellPackages.ghcWithPackages (hs: with hs;[xmonad xmonad-contrib xmonad-extras])" --run "ghci"
+nix-shell -p "(import <nixos> {}).haskellPackages.ghcWithPackages (hs: with hs;[xmonad xmonad-contrib xmonad-extras])" --run "ghci"
