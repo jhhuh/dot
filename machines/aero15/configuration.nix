3aa08de6ca3728dcc686a22b2c017c14ced2dae6
Ji-Haeng Huh <jhhuh.note@gmail.com>
2022-02-06 04:48:22 +0900

    add machine/aero15

---
+++ machines/aero15/configuration.nix
@@ -0,0 +1,195 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking = {
+    hostName = "aero15";
+
+    networkmanager.enable = true;
+
+    useDHCP = false;
+
+    interfaces = {
+      "enp3s0".useDHCP = true;
+      "wlp4s0".useDHCP = true;
+    };
+
+    firewall = {
+      allowedTCPPortRanges = [{ from = 1714; to = 1764; }]; # KDE connect
+      allowedUDPPortRanges = [{ from = 1714; to = 1764; }]; # KDE connect
+      allowedTCPPorts = [ 4001 ]; # ipfs
+      allowedUDPPorts = [ 4001 ]; # ipfs
+      checkReversePath = false;
+    };
+  };
+
+  time.timeZone = "Asia/Seoul";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [hangul];
+    };
+  };
+
+  console = {
+    useXkbConfig = true;
+  };
+
+  security.pam.enableSSHAgentAuth = true;
+
+  systemd.tmpfiles.rules = let
+    cfg = config.services.ipfs;
+  in [
+    "f '${cfg.dataDir}/config' 0644 ${cfg.user} ${cfg.group} - -"
+  ];
+
+  services = {
+    ipfs = {
+      enable = true;
+      localDiscovery = true;
+      autoMount = true;
+    };
+    kmscon.enable = true;
+    kmscon.hwRender = true;
+
+    redis.enable = true;
+
+    udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
+
+    xserver = {
+      enable = true;
+
+      layout = "us";
+
+      dpi = 192;
+
+      #videoDrivers = ["nvidia"];
+
+      libinput.enable = true;
+
+      xkbOptions = "ctrl:swapcaps";
+
+      desktopManager = {
+        plasma5.enable = false;
+        gnome.enable = true;
+      };
+
+      displayManager = {
+        gdm = {
+          enable = true;
+          wayland = false;
+       #   nvidiaWayland = true;
+        };
+       # sessionCommands = ''
+       #   ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource modesetting NVIDIA-0
+       #   ${pkgs.xorg.xrandr}/bin/xrandr --auto
+       # '';
+      };
+
+      useGlamor = true;
+
+      #imwheel = {
+      #  enable = true;
+      #  rules = {
+      #    ".*" = ''
+      #      None,      Up,   Button4, 10
+      #      None,      Down, Button5, 10
+      #    '';
+      #  };
+      #};
+    };
+    printing.enable = true;
+    openssh.enable = true;
+    tailscale.enable = true;
+  };
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-extra
+    noto-fonts-cjk
+    ubuntu_font_family
+    hack-font
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      bash-prompt-suffix = \[\033[1;33m\]\n(nix develop)\$ \[\033[0m\]
+      experimental-features = nix-command flakes
+    '';
+    trustedUsers = [ "root" "jhhuh" ];
+  };
+
+  # Enable sound.
+  sound.enable = true;
+
+  hardware = {
+    bluetooth.enable = true;
+    enableAllFirmware = true;
+    pulseaudio = {
+      enable = true;
+      extraModules = [pkgs.pulseaudio-modules-bt];
+      package = pkgs.pulseaudioFull;
+    };
+    video.hidpi.enable = true;
+   # nvidia = {
+   #   prime = {
+   #     sync.enable = true;
+   #     intelBusId = "PCI:0:2:0";
+   #     nvidiaBusId = "PCI:1:0:0";
+   #   };
+   #   modesetting.enable = true;
+   # };
+  };
+
+  #powerManagement.powertop.enable = true;
+  #powerManagement.cpuFreqGovernor = "performance";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."jhhuh" = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" ]; # Enable ‘sudo’ for the user.
+  };
+
+  environment.systemPackages = with pkgs; [
+    nixos-option
+    vimHugeX git tmux
+  ];
+
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
+    libvirtd.enable = true;
+    virtualbox.host = {
+      enable = false;
+      enableExtensionPack = true;
+    };
+  };
+
+  programs = {
+    ssh.startAgent = true;
+    slock.enable = true;
+  };
+
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
+
--- machines/flake.nix
+++ machines/flake.nix
@@ -10,13 +10,30 @@
 
   outputs = inputs@{ self, nixpkgs, utils, deploy-rs }: {
     nixosConfigurations = {
+
+      aero15 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./aero15/configuration.nix ];
+      };
+
       tres-cantos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./tres-cantos/configuration.nix ];
       };
+
     };
 
     deploy.nodes = {
+
+      aero15 = {
+        hostname = "aero15.jhhuh-korea.gmail.com.beta.tailscale.net";
+        sshOpts = [ "-A" ];
+        profiles."system" = {
+          user = "root";
+          path = deploy-rs.lib."x86_64-linux".activate.nixos self.nixosConfigurations.aero15;
+        };
+      };
+
       tres-cantos = {
         hostname = "tres-cantos.jhhuh-korea.gmail.com.beta.tailscale.net";
         sshOpts = [ "-A" ];
@@ -25,6 +42,7 @@
           path = deploy-rs.lib."x86_64-linux".activate.nixos self.nixosConfigurations.tres-cantos;
         };
       };
+
     };
   } // utils.lib.eachSystem ["x86_64-linux"] (system:
   let
