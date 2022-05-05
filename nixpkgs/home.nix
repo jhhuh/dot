848ac2b4616425474f9c9e4f8228067c349f88fe
Ji-Haeng Huh <jhhuh.note@gmail.com>
2022-05-05 09:51:53 +0900

    add touchpad toggle script

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -4,6 +4,18 @@ let
 in rec {
   home = {
     packages = (with pkgs; [
+      (writeScriptBin "toggle_touchpad.sh"
+       ''
+         #!{bash}/bin/bash
+         
+         if [ $(gsettings get org.gnome.desktop.peripherals.touchpad send-events) == "'enabled'" ]; then
+           echo "Switching off"
+           gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled
+else     
+           echo "Switching on"
+         gsettings set org.gnome.desktop.peripherals.touchpad send-events enabled
+         fi
+       '')
       qemu
       xclip
       steam-run
