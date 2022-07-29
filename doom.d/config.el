2bc341f4787922dc617ef07dcd5ef19e8f61c036
Ji-Haeng Huh <jhhuh.note@gmail.com>
2022-07-29 12:41:42 +0900

    comment out some doom customization

--- doom.d/config.el
+++ doom.d/config.el
@@ -19,9 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Ubuntu Mono" :size 36)
-      doom-big-font (font-spec :family "Ubuntu Mono" :size 48)
-      doom-variable-pitch-font (font-spec :family "sans" :size 32))
+;;(setq doom-font (font-spec :family "Ubuntu Mono" :size 36)
+;;      doom-big-font (font-spec :family "Ubuntu Mono" :size 48)
+;;      doom-variable-pitch-font (font-spec :family "sans" :size 32))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -54,8 +54,8 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq which-key-idle-delay 0.01)
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;;(setq which-key-idle-delay 0.01)
+;;(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
 (after! lsp-ui
   (setq lsp-ui-doc-position 'top))
