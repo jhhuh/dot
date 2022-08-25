4a01e945c7feca91155ad82be5691e3e5998931f
Ji-Haeng Huh <jhhuh.note@gmail.com>
2022-08-26 02:00:47 +0900

    change emacs font size

--- doom.d/config.el
+++ doom.d/config.el
@@ -19,9 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Ubuntu Mono" :size 32)
+(setq doom-font (font-spec :family "Ubuntu Mono" :size 24)
      doom-big-font (font-spec :family "Ubuntu Mono" :size 48)
-     doom-variable-pitch-font (font-spec :family "sans" :size 32))
+     doom-variable-pitch-font (font-spec :family "sans" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
