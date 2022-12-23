11449ab8f4081adb5b72b5f33ded9235593044db
Ji-Haeng Huh <jhhuh.note@gmail.com>
2022-12-23 19:26:18 +0900

    remove old way of pinning nixpkgs

--- update_nix-channel-git-revision.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-channel="nixos-21.05"
-rev=$(curl -L https://nixos.org/channels/"$channel"/git-revision | tee ./git-revision_nixpkgs)
-
-source ./update_nix-channel.sh
--- update_nix-channel.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-nix-channel --add https://github.com/nixos/nixpkgs/archive/$(<git-revision_nixpkgs).tar.gz nixpkgs
-nix-channel --list
-nix-channel --update nixpkgs
