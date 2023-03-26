49da8434d5dbcc0c82bcfc01acd60c4566e13ca5
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-03-27 01:24:14 +0900

    add floating keys controlling floating windows

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -39,7 +39,13 @@ import XMonad.Layout.Gaps (
   Direction2D(..),
   GapMessage(..))
 
+import XMonad.Actions.FloatKeys (keysMoveWindow, keysResizeWindow)
+
 import qualified Data.Map as M
+
+import Data.Maybe (fromMaybe)
+import Data.Ratio ((%))
+import Control.Monad (guard)
 
 import XMonad.Hooks.EwmhDesktops (
   ewmh,
@@ -54,8 +60,8 @@ desktop = ewmhFullscreen . ewmh $ def {
  }
 
 conf = desktop {
-  borderWidth = 0,
-  focusedBorderColor = "#ff8267",
+  borderWidth = 4,
+  focusedBorderColor = "#00FF00",
   terminal = "xst",
   modMask = mod4Mask,
   keys = keys desktop <+> myKeys,
@@ -131,7 +137,90 @@ myKeys XConfig { modMask = modm }
       ( (modm .|. controlMask, xK_d ),
         sendMessage $ DecGap 5 D ),
       ( (modm .|. controlMask, xK_x ),
-        xmonadPrompt def ) ]
+        xmonadPrompt def )
+      -- Floating Window resizing
+      , ( (modm .|. shiftMask, xK_u ), withFocused $ keysResizeWindow (0, 10) (0, 0)) -- enlarge down
+      , ( (modm .|. shiftMask, xK_i ), withFocused $ keysResizeWindow (0, -10) (0, 0)) -- shrink down
+      , ( (modm .|. shiftMask, xK_y ), withFocused $ keysResizeWindow (-10, 0) (0, 0)) -- shrink right
+      , ( (modm .|. shiftMask, xK_o ), withFocused $ keysResizeWindow (10, 0) (0, 0)) -- enlarge right
+      , ( (modm .|. controlMask, xK_u ), withFocused $ keysResizeWindow (0, -10) (0, 1)) -- shrink up
+      , ( (modm .|. controlMask, xK_i ), withFocused $ keysResizeWindow (0, 10) (0, 1)) -- enlarge up
+      , ( (modm .|. controlMask, xK_y ), withFocused $ keysResizeWindow (10, 0) (1, 0)) -- enlarge left
+      , ( (modm .|. controlMask, xK_o ), withFocused $ keysResizeWindow (-10, 0) (1, 0)) -- shrink left
+      -- Floating Window moving
+      , ( (modm, xK_i), withFocused $ keysMoveWindow (0, -9))
+      , ( (modm, xK_u), withFocused $ keysMoveWindow (0, 9))
+      , ( (modm, xK_o), withFocused $ keysMoveWindow (16, 0))
+      , ( (modm, xK_y), withFocused $ keysMoveWindow (-16, 0))
+      -- Window floating at a custom position
+      , ( (modm, xK_z), withFocused $ floatToRationalRect myLeft)
+      , ( (modm, xK_x), withFocused $ floatToRationalRect myCenter)
+      , ( (modm, xK_c), withFocused $ floatToRationalRect myRightCenter)
+      , ( (modm, xK_v), withFocused $ floatToRationalRect myRight)
+      , ( (modm, xK_b), withFocused $ floatToRationalRect myFull)
+    ]
+
+
+myCenter :: W.RationalRect
+myCenter = W.RationalRect (4 / 32) (1 / 32) (24 / 32) (30 / 32)
+
+
+myFull :: W.RationalRect
+myFull = W.RationalRect (0 / 32) (0 / 32) (32 / 32) (32 / 32)
+
+
+myRight :: W.RationalRect
+myRight = W.RationalRect (33 / 64) (1 / 32) (15 / 32) (30 / 32)
+
+
+myLeft :: W.RationalRect
+myLeft = W.RationalRect (1 / 64) (1 / 32) (15 / 32) (30 / 32)
+
+
+myTopCenter :: W.RationalRect
+myTopCenter = W.RationalRect (1 / 32) (1 / 32) (30 / 32) (16 / 32)
+
+
+myRightCenter :: W.RationalRect
+myRightCenter = W.RationalRect (10 / 32) (1 / 32) (21 / 32) (30 / 32) -- px py wx wy
+
+
+floatToRationalRect :: W.RationalRect -> Window -> X ()
+floatToRationalRect rr' w = do
+  floats <- gets (W.floating . windowset)
+  (sc, rr) <- floatLocation w
+  if w `M.member` floats && rr `almostSame` rr' -- if the current window is floating...
+    then (windows . W.sink) w
+    else do
+      windows $ \ws -> W.float w rr' . fromMaybe ws $ do
+        i <- W.findTag w ws
+        guard $ i `elem` map (W.tag . W.workspace) (W.screens ws)
+        f <- W.peek ws
+        sw <- W.lookupWorkspace sc ws
+        return (W.focusWindow f . W.shiftWin sw w $ ws)
+  where
+    almostSame :: W.RationalRect -> W.RationalRect -> Bool
+    almostSame (W.RationalRect px py wx wy) (W.RationalRect px' py' wx' wy') =
+      rd px px' <= tol
+        && rd py py' <= tol
+        && rd wx wx' <= tol
+        && rd wy wy' <= tol
+      where
+        tol = 1 % 100
+        rd a b = 2 * abs (a - b) / (a + b) -- relative difference
+
+
+-- If the window is floating then (f), if tiled then (n)
+ifFloatThenElse :: X () -> X () -> X ()
+ifFloatThenElse f n = withFocused $ \windowId -> do
+  floats <- gets (W.floating . windowset)
+  if windowId `M.member` floats -- if the current window is floating...
+    then f
+    else n
+
+
+ifFloatThenSinkElse :: X () -> X ()
+ifFloatThenSinkElse = ifFloatThenElse (withFocused $ windows . W.sink)
 
 scratchpads =
   map makeNS [
@@ -216,3 +305,4 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask}
 
 main :: IO ()
 main = xmonad =<< statusBar myBar myXmobarPP toggleStrutsKey conf
+
