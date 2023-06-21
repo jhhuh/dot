fc97fad62b33673c0abb249bcab50a5cd05c5efb
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-06-21 17:17:55 +0900

    remove mimir from builder-spec.nix

--- machines/builder-specs.nix
+++ machines/builder-specs.nix
@@ -15,10 +15,16 @@ in
 
   {
 
-    mimir = common-spec // {
-      hostName = "mimir.coati-bebop.ts.net";
-      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdDbXFITlVmOFd4OGd5blk4MzJBZTc2QjNuWFhOS2RvZ2N5L2c4SkZDRWMgcm9vdEBtaW1pcgo=";
+    cafe = common-spec // {
+      hostName = "cafe.coati-bebop.ts.net";
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtnYXlTakVLaDBjUTNVdlR3WVFzZ3E0Wm1OTFcwdXU2R2RmTDZhMWxYTW8gcm9vdEBjb21tb24K";
       protocol = "ssh-ng";
     };
+
+    # mimir = common-spec // {
+    #   hostName = "mimir.coati-bebop.ts.net";
+    #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdDbXFITlVmOFd4OGd5blk4MzJBZTc2QjNuWFhOS2RvZ2N5L2c4SkZDRWMgcm9vdEBtaW1pcgo=";
+    #   protocol = "ssh-ng";
+    # };
 
   }
