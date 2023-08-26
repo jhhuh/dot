43d158220542e4acc433674519eafebd6d2354a5
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-08-26 18:27:01 +0900

    bump up to 23.05

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
 
-    nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
 
-    home-manager.url = github:nix-community/home-manager/release-22.11;
+    home-manager.url = github:nix-community/home-manager/release-23.05;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     declarative-cachix.url = github:jonascarpay/declarative-cachix;
