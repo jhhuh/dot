86925b6163a9b0caa34889f4f947bc5b86d3c673
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-12-27 05:05:08 +0900

    bump up flake

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
 
-    nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-23.11;
 
-    home-manager.url = github:nix-community/home-manager/release-23.05;
+    nixpkgs-unstable.url = github:nixos/nixpkgs/nixos-unstable;
+
+    home-manager.url = github:nix-community/home-manager/release-23.11;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     declarative-cachix.url = github:jonascarpay/declarative-cachix;
