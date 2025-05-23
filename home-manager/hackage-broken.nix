3254cc2aa996fe496a059ffa43b832269af06b78
Ji-Haeng Huh <jhhuh.note@gmail.com>
2025-05-23 10:53:29 +0900

    remove ghc megapackage

--- home-manager/hackage-broken.nix
+++
@@ -1,5 +0,0 @@
-{ runCommand, jq, candidates-txt ? ./hackage-blacklist-candidates.txt }:
-
-runCommand "hackage-broken.json" { buildInputs = [ jq ]; } ''
-  cat ${candidates-txt} | uniq | jq '[., inputs]' > $out
-''
