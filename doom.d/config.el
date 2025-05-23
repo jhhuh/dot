0fa8b4246dd268a043ea6dd335ecf468965b075e
Ji-Haeng Huh <jhhuh.note@gmail.com>
2025-05-23 10:50:38 +0900

    update doom config

--- doom.d/config.el
+++ doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Ji-Haeng Huh"
-      user-mail-address "jhhuh.korea@gmail.com")
+      user-mail-address "jhhuh.note@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -15,28 +15,19 @@
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-
-(cond ((string= (system-name) "cafe")
-       (setq doom-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 14.0 :weight 'regular)
-             doom-big-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 16.0)
-             doom-variable-pitch-font (font-spec :family "sans" :size 14.0)))
-      ((string= (system-name) "x230")
-       (setq doom-font (font-spec :family "Ubuntu Nerd Font Mono" :size 16.0 :weight 'regular)
-             doom-variable-pitch-font (font-spec :family "sans" :size 12.0)))
-      ((string= (system-name) "p1gen3")
-       (setq doom-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 16.0)
-             doom-big-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 24.0)
-             doom-variable-pitch-font (font-spec :family "sans" :size 16.0)))
-      (t
-       (setq doom-font (font-spec :family "Ubuntu Nerd Font Mono" :size 16.0 :weight 'regular)
-             doom-variable-pitch-font (font-spec :family "sans" :size 12.0))))
-
+(setq doom-font (font-spec
+                 :family "Iosevka Nerd Font Mono"
+                 :size 32)
+      doom-variable-pitch-font (font-spec
+                                :family "Noto Sans CJK KR"
+                                :size 32))
+;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
@@ -45,16 +36,17 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-theme 'doom-monokai-machine)
+
+(doom/set-frame-opacity 80)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -87,150 +79,66 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+(advice-add #'lsp! :override #'ignore)
 
 (after! persp-mode
-  (setq persp-emacsclient-init-frame-behaviour-override "main")
+  (setq persp-emacsclient-init-frame-behaviour-override "emacsclient"))
 
-  (defun workspaces-formatted ()
-    (+doom-dashboard--center (frame-width) (+workspace--tabline)))
+(setq doom-reload-command "EMACS=emacs %s sync -B -e")
 
-  (defun hy/invisible-current-workspace ()
-    (propertize (safe-persp-name (get-current-persp)) 'invisible t))
+(after! envrc
+  (setq envrc-remote t))
 
-  (customize-set-variable 'tab-bar-format '(+workspace--tabline tab-bar-format-align-right hy/invisible-current-workspace))
+(after! doom-modeline
+  (setq doom-modeline-persp-name t))
 
-  (advice-add #'+workspace/display :override #'ignore)
-  (advice-add #'+workspace-message :override #'ignore))
-
-(run-at-time nil nil (cmd! (tab-bar-mode +1)))
-
-(use-package! envrc
-  :config
-  (setq envrc-debug t)
-  :hook (after-init . envrc-global-mode))
-
-(use-package! lsp-mode
-  :custom
-  (lsp-completion-enable-additional-text-edit nil)
-  :config
-  (setq lsp-use-plists "true")
-  :hook (lsp-mode . (lambda ()
-                      (lsp-ui-mode)
-                      (lsp-ui-doc-mode))))
-
-(use-package! lsp-haskell
-  :hook (haskell-mode . (lambda ()
-                          (lsp-deferred)
-                          (setq-default flycheck-disabled-checkers '(haskell-stack-ghc)))))
-
-(use-package! lsp-ui
-  :config
-  (setq lsp-ui-doc-show-with-cursor t)
-  (setq lsp-ui-doc-position 'top))
-
-(setq company-idle-delay nil)
-
-(setq which-key-idle-delay 0.01)
-
-(setq doom-modeline-persp-name t)
-
-(defun on-after-init ()
-  (unless (display-graphic-p (selected-frame))
-    (set-face-background 'default "unspecified-bg" (selected-frame))))
-
-(add-hook 'server-after-make-frame-hook 'on-after-init)
-
-(use-package! auth-source
-  :config
+(after! auth-source
   (setq! auth-sources '(password-store))
   (setq! auth-source-cache-expiry nil))
 
-(use-package! gptel
-  :config
-  (setq! gptel-default-mode #'org-mode
-        gptel-model "gpt-4o"
-        gptel-temperature 0.0)
-  )
-
 (use-package! gptel-extensions)
 
-(use-package! codegpt
-  :ensure t
-  :config
-  (setq! openai-key #'openai-key-auth-source))
+(after! gptel
+  (setq! gptel-default-mode #'org-mode
+         gptel-model 'gpt-4o
+         gptel-temperature 0.0))
 
-(use-package! codegpt
-  :ensure t
-  :config
-  (setq! codegpt-tunnel 'chat            ; The default is 'completion
-        codegpt-model "gpt-")) ; You can pick any model you want!
+;; (after! magit-gptcommit
+;;     :after magit
+;;     :bind (:map git-commit-mode-map
+;;                 ("C-c C-g" . magit-gptcommit-commit-accept))
+;;     :init
+;;     (defun my/magit-gptcommit-openai-llm-provider ()
+;;       (let (provider)
+;;         (lambda ()
+;;           (or provider
+;;               (progn
+;;                 (require 'llm-openai)
+;;                 (setq provider (make-llm-openai
+;;                                 :key
+;;                                 (auth-info-password
+;;                                  (car (auth-source-search
+;;                                        :host (gptel-backend-host gptel-backend)
+;;                                        :user "apikey"
+;;                                        :require '(:secret)))))))))))
+;;     :custom
+;;     (magit-gptcommit-llm-provider (my/magit-gptcommit-openai-llm-provider))
 
-(use-package! chatgpt
-  :ensure t
-  :config
-  (setq! chatgpt-model "gpt-4"))
+;;     :config
+;;     (magit-gptcommit-mode 1)
+;;     (magit-gptcommit-status-buffer-setup))
 
-;; Upstream dash-docs is broken for async functionality
-(defun dash-docs-async-install-docset (docset-name)
-  "Asynchronously download docset with specified DOCSET-NAME and move its stuff to docsets-path."
-  (interactive (list (dash-docs-read-docset "Install docset" (dash-docs-official-docsets))))
-  (when (dash-docs--ensure-created-docsets-path (dash-docs-docsets-path))
-    (let ((feed-url (format "%s/%s.xml" dash-docs-docsets-url docset-name)))
-      (message (concat "The docset \"" docset-name "\" will now be installed asynchronously."))
-      (async-start ; First async call gets the docset meta data
-       `(lambda ()
-          ;; Beware! This lambda is run in it's own instance of emacs.
-          (url-file-local-copy ,feed-url))
-       (lambda (filename)
-         (let ((docset-url (dash-docs-get-docset-url filename)))
-           (async-start     ; Second async call gets the docset itself
-            `(lambda ()
-               ;; Beware! This lambda is run in it's own instance of emacs.
-               (url-file-local-copy ,docset-url))
-            (lambda (docset-tmp-path)
-              (let ((docset-folder (dash-docs-extract-and-get-folder docset-tmp-path)))
-                (dash-docs-activate-docset docset-folder)
-                (message (format
-                          "Docset installed. Add \"%s\" to dash-docs-common-docsets or dash-docs-docsets."
-                          docset-folder)))))))))))
 
-;(use-package! org-ai
-;  :commands (org-ai-mode org-ai-global-mode)
-;  :init
-;  (add-hook 'org-mode-hook #'org-ai-mode)
-;  (org-ai-global-mode)
-;  :config
-;  ;(setq org-ai-default-chat-model "gpt-4")
-;  (setq org-ai-use-auth-source t)
-;  (org-ai-install-yasnippets))
-
-;(use-package! greader)
-
-;; (use-package! lsp-grammarly
-;;   :ensure t
-;;   :hook (text-mode . (lambda ()
-;;                        (require 'lsp-grammarly)
-;;                        (lsp))))  ; or lsp-deferred
-
-;; accept completion from copilot and fallback to company
 (use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :config  (setq! copilot-indent-offset-warning-disable t)
-  :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
-
-(after! (evil copilot)
-  ;; Define the custom function that either accepts the completion or does the default behavior
-  (defun my/copilot-tab-or-default ()
-    (interactive)
-    (if (and (bound-and-true-p copilot-mode)
-             ;; Add any other conditions to check for active copilot suggestions if necessary
-             )
-        (copilot-accept-completion)
-      (evil-insert 1))) ; Default action to insert a tab. Adjust as needed.
-
-;; Bind the custom function to <tab> in Evil's insert state
-(evil-define-key 'insert 'global (kbd "<tab>") 'my/copilot-tab-or-default))
+ :bind (:map copilot-mode-map
+        ("<tab>" . #'copilot-accept-completion)
+        ("C-<tab>" . #'copilot-accept-completion-by-word)
+        ("C-!" . #'copilot-next-completion)
+        :map copilot-completion-map
+        ("<tab>" . #'copilot-accept-completion))
+  :config
+  (setq! copilot-indent-offset-warning-disable t
+   copilot-idle-delay 0.12
+   copilot-max-char -1
+   copilot-log-max 10000))
--- doom.d/init.el
+++ doom.d/init.el
@@ -21,108 +21,107 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company             ; the ultimate code completion backend
+       ;;company           ; the ultimate code completion backend
+       (corfu +icons +orderless +dabbrev)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)    ; the search engine of the future
+       (vertico +icons +childframe)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom                ; what makes DOOM look the way it does
-       doom-dashboard      ; a nifty splash screen for Emacs
-       doom-quit           ; DOOM quit-message prompts when you quit Emacs
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
-       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
-       minimap             ; show a map of the code on the side
-       modeline            ; snazzy, Atom-inspired modeline, plus API
-       nav-flash           ; blink cursor line after big motions
+       ;;minimap           ; show a map of the code on the side
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
-       ophints             ; highlight the region an operation acts on
-       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ophints           ; highlight the region an operation acts on
+       (popup +all +defaults)   ; tame sudden yet inevitable temporary windows
+       (smooth-scroll +interpolate)     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
-       treemacs            ; a project drawer, like neotree but cooler
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
-       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces          ; tab emulation, persistence & separate workspaces
-       zen                 ; distraction-free coding or writing
+       workspaces        ; tab emulation, persistence & separate workspaces
+       zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere)  ; come to the dark side, we have cookies
-       file-templates      ; auto-snippets for empty files
-       fold                ; (nigh) universal code folding
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
-       ;;lispy             ; vim for lisp, for people who don't like vim
+       lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets            ; my elves. They type so I don't have to
+       snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
+       eww               ; the internet is gross
+       ;;ibuffer           ; interactive buffer management
+       (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm               ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
+       (syntax +icons +flymake +childframe)              ; tasing you for every semicolon you forget
        ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
-       ;;(collab +tuntox)           ; buffers with friends
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ein                 ; tame Jupyter notebooks with emacs
+       ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
-       (lookup +dictionary +docsets)            ; navigate your code and its documentation
-       lsp                 ; M-x vscode
-       magit               ; a git porcelain for Emacs
+       (lookup +dictionary +docsets +offline)              ; navigate your code and its documentation
+       llm               ; when I said you needed friends, I didn't mean...
+       (lsp +eglot)              ; M-x vscode
+       magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       (pass +auth)        ; password manager for nerds
+       (pass +auth)              ; password manager for nerds
        pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
-       (tty +osc)          ; improve the terminal Emacs experience
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)           ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
-       common-lisp         ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
@@ -138,47 +137,64 @@
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
-       (haskell +lsp)      ; a language that's lazier than I am
+       (haskell +lsp +tree-sitter)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
-       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (json +tree-sitter)             ; At least it ain't XML
+       (java +lsp)       ; the poster child for carpal tunnel syndrome
+       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown            ; writing docs for people to ignore
+       (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       nix                 ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org                 ; organize your plain life in plain text
+       (org
+        +brain
+        +contacts
+        +dragndrop
+        +crypt
+        +gnuplot
+        +hugo
+        +journal
+        +jupyter
+        +noter
+        +pandoc
+        +passwords
+        +pomodoro
+        +present
+        +pretty
+        +roam2)            ; organize your plain life in plain text
+
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python +lsp +cython +pyright +tree-siter)  ; beautiful is better than ugly
+       (python +tree-sitter +lsp)          ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails)       ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                  ; she sells {ba,z,fi}sh shells on the C xor
+       sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
-       ;;(mu4e +org +gmail)
+       (mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
@@ -188,22 +204,7 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
-       (default +bindings +smartparens))
-
-(setenv "LSP_USE_PLISTS" "1")
-
-(setq native-comp-jit-compilation nil)
-(after! (doom-packages straight)
-  (setq straight--native-comp-available t))
-
-(after! comp
-  ;; HACK Disable native-compilation for some troublesome packages
-  (mapc (doom-partial #'add-to-list 'native-comp-deferred-compilation-deny-list)
-        (list "/emacs-jupyter.*\\.el\\'"
-              "/evil-collection-vterm\\.el\\'"
-              "/vterm\\.el\\'"
-              "/with-editor\\.el\\'")))
+       (default +bindings +gnupg +smartparens))
